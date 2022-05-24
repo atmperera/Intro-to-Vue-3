95d7a6250fdbd33691cf9b3f798b745048ccd9f4
atmperera <53685019+atmperera@users.noreply.github.com>
2022-05-24 19:19:08 +0530

    onSale finished

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
+<<<<<<< Updated upstream
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester']
+=======
+            onSale:true
+>>>>>>> Stashed changes
         }
     }
 })
